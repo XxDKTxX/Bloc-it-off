75c69485a014516bce8adc235882481cf1060f80
Devin <devintassie@gmail.com>
2018-08-20 21:07:12 -0500

    working on hiding tasks

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,16 +17,40 @@
 
      angular
          .module('ToDo', ['ui.router', 'firebase'])
+
          .config(config)
+
          .controller('todoController',["$scope", "$firebaseArray",
            function($scope, $firebaseArray) {
+
              var ref = firebase.database().ref(todos);
              var todos = $firebaseArray(ref);
+
+             $scope.date = new Date();
+
+            //  $scope.dateFilter = function(todo){
+            //     if (todo.created == 1534807668513 ) {
+            //       return true;
+            //   };
+            // };
+
+
+             // $scope.shouldShow = function(todo) {
+             //   if (todo.shouldShow === true || todo.shouldShow === undefined) {
+             //     return true
+             //   } else {
+             //     return false
+             //   }
+             // }
+             //
+             // $scope.hideTodo = function(todo) {
+             //   todo.shouldShow = false
+             // }
 
              $scope.todos = todos
 
              $scope.addTodo = function(){
-              $scope.todos.$add({'title': $scope.newtodo, 'done':false})
+              $scope.todos.$add({'title': $scope.newtodo, 'done': false, 'created': firebase.database.ServerValue.TIMESTAMP })
                $scope.newtodo = ''
            };
 
@@ -35,8 +59,20 @@
                console.log(item, index)
                 if (item.done) {
                  $scope.todos.$remove(index);
-               }
-             })
+               };
+             });
            };
+
+
          }]);
  })();
+
+
+ // .filter("weekFilter", function(){
+ //   return function(input,optionalParam1, optionalParam2){
+ //
+ //     var output;
+ //       // custom code here...
+ //     return output;
+ //   };
+ // });
